c309af36f3501876dd7f98922da7780a0bf33cc8
Jiahua Liao <ljh15088131916@gmail.com>
2020-08-01 23:04:53 -0400

    update project2

--- JS/project2.js
+++ JS/project2.js
@@ -29,3 +29,32 @@ input.onkeyup=function (e) {
 //     alert(typeof value+' '+value);
 // }
 
+let bankNum = document.getElementById('bankNum');
+let output2 = document.getElementsByClassName('output')[1];
+output2.innerHTML = "Please enter a bank number wiht 16 digits";
+bankNum.onkeyup=function(e){
+    let code=this.value.replace(/[^0-9]+/g,'')
+    let len = code.length
+
+    if(this.value.length === 4 || this.value.length === 9 || this.value.length === 14 && e.keyCode !== 8){
+        this.value=this.value+' ';
+    }
+    if(e.keyCode === 8) {
+        output2.innerHTML = "Please enter a bank number wiht 16 digits";
+        output2.style.color='black';
+    }
+    if(len === 16){
+        let arr = (code + '').split('').reverse().map(x => parseInt(x));
+        let lastDigit = arr.splice(0, 1)[0];
+        let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
+        sum += lastDigit;
+        if(sum%10 === 0) {
+            output2.style.color='green';
+            output2.innerHTML = "Valid Number";       
+        }
+        else {
+            output2.style.color='red';
+            output2.innerHTML = "Invalid Number";         
+        }
+    }
+}
